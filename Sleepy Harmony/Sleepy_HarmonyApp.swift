d6220977a7babf4053025221e21e46c7daaf7e90
Mansur <idrisovmansur00@gmail.com>
2024-04-18 22:41:09 +0500

    first commit

---
+++ Sleepy Harmony/Sleepy_HarmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleepy_HarmonyApp.swift
+//  Sleepy Harmony
+//
+//  Created by Mansur Ydyrys on 15.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Sleepy_HarmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sleepy Harmony/bio-rythms.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+import AVFoundation
+
+struct bio_rythms: View {
+    @StateObject var audioPlayerViewModel = AudioPlayerViewModel()
+
+    var body: some View {
+        VStack {
+            Button(action: {
+                audioPlayerViewModel.playOrPause()
+            }) {
+                Image(systemName: audioPlayerViewModel.isPlaying ? "pause.circle" : "play.circle")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+        }
+    }
+}
+
+#Preview {
+    bio_rythms()
+}
+
+class AudioPlayerViewModel: ObservableObject {
+  var audioPlayer: AVAudioPlayer?
+
+  @Published var isPlaying = false
+
+  init() {
+    if let sound = Bundle.main.path(forResource: "forest", ofType: "mp3") {
+      do {
+        self.audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound))
+      } catch {
+        print("AVAudioPlayer could not be instantiated.")
+      }
+    } else {
+      print("Audio file could not be found.")
+    }
+  }
+
+  func playOrPause() {
+    guard let player = audioPlayer else { return }
+
+    if player.isPlaying {
+      player.pause()
+      isPlaying = false
+    } else {
+      player.play()
+      isPlaying = true
+    }
+  }
+}
